cf1e7b0002229c8dc48f921cd01d2baea8c42d1d
kavyareddypatil <kavyareddypatil531@gmail.com>
2026-09-17 11:49:04 +0530

    Prepare Django settings for deployment

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--*3^%s#d-8lu2mo^d!cv!spvsdxnaob*9a%tfhag&ktg$rqt8$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Email
